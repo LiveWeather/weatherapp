1c0af3ea2004c8525294332486f88da2dc82c462
Ashish-Kumar-Singh <blr.ashu@gmail.com>
2017-12-10 21:54:31 +0000

    final commit

---
+++ styleindex.php
@@ -0,0 +1,43 @@
+* {
+  border: 0px;
+  margin: 0px;
+  letter-spacing: 1.5px;
+}
+
+li a{
+	color:black;
+}
+
+li a:hover{
+	background-color:black;
+	text-decoration:none;
+}
+
+search {
+  text-align: center;
+}
+
+mycarousel {
+  letter-spacing: 1.5px;
+}
+#temp{
+	text-align:right;
+}
+.warn{
+  color:red;
+  font-size:10px;
+}
+.success {
+	color: #3c763d; 
+	background: #dff0d8; 
+	border: 1px solid #3c763d;
+	margin-bottom: 20px;
+}
+.footer{
+  vertical-align: middle;
+  background-color:gray;
+	color:white;
+	min-height:70px;
+	text-align:center;
+	font-size:15px;
+}
