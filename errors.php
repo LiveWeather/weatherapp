42aa0e8e62fc737b2a9114abe521131a10d5432c
Ashish-Kumar-Singh <blr.ashu@gmail.com>
2017-12-10 21:50:19 +0000

    final commit

--- errors.php
+++ errors.php
@@ -1,8 +1,10 @@
-<?php  if (count($errors) > 0) : ?>
+<?php
+//Reference-https://codewithawa.com/posts/complete-user-registration-system-using-php-and-mysql-database
+//Youtube-Tutorial-https://www.youtube.com/watch?v=C--mu07uhQw
+if (count($errors) > 0) : ?>
 	<div class="error">
 		<?php foreach ($errors as $error) : ?>
 			<p><?php echo $error ?></p>
 		<?php endforeach ?>
 	</div>
 <?php  endif ?>
-
